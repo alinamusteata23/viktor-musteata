993cf3e6c632cf228c65da063789c61ff5742375
Nataliia <natalabojko@gmail.com>
2025-02-21 13:56:43 +0200

    burger hover

--- src/components/buttons/BurgerBtn/BurgerBtn.jsx
+++ src/components/buttons/BurgerBtn/BurgerBtn.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./BurgerBtn.module.scss";
 
 const BurgerBtn = () => {
+  const [burgerIsClicked, setBurgerIsClicked] = useState(false);
+
+  const burgerButnToggle = () => {
+    setBurgerIsClicked(!burgerIsClicked);
+  };
+
   return (
-    <button className={styles.burgerBtn}>
+    <button
+      className={
+        burgerIsClicked
+          ? `${styles.burgerBtn} ${styles.btnIsClicked}`
+          : styles.burgerBtn
+      }
+      onClick={burgerButnToggle}
+    >
       <div className={styles.middleLine}></div>
     </button>
   );
