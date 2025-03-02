e642ad737256979c6cd8b3d1ecac929659a33adf
Maksimchik36 <Peltek1985@gmail.com>
2025-03-02 11:49:01 +0200

    returned back start settings, created sectionsData

--- src/sections/AboutUs/AboutUs.jsx
+++ src/sections/AboutUs/AboutUs.jsx
@@ -1,37 +1,33 @@
-import React from "react";
-import styles from "./AboutUs.module.scss";
 import Image from "next/image";
+import {sectionsData} from '@/data/sectionsData';
+import styles from "./AboutUs.module.scss";
 
 
 const AboutUs = () => {
+  const data = sectionsData.filter(section=> section.name === "AboutUs");
+  console.log('data', data)
+
+
   return <section>
     <div className={`container ${styles.container}`}>
-      <h2 className="sectionTitle">Über uns</h2>
+      <h2 className="sectionTitle">{data?.title}</h2>
       <div className={styles.infoWrapper}>
         <Image 
         className={styles.img}
-        src="/imgs/about-us.webp"
-        alt="Master's photo"
+        src={data?.imgSrc}
+        alt={data?.imgAlt}
         width={280}
         height={334}/>
-        <p className={styles.infoText}>Wir bieten professionelle Fliesenverlegung für Sie, um Ihrem Raum ein modernes und elegantes Aussehen zu verleihen. Mit hochwertigen Materialien und präziser Ausführung sorgen wir für langlebige und wasserdichte Ergebnisse. Vertrauen Sie auf unsere Erfahrung, um Ihren Raum in einen stilvollen Rückzugsort zu verwandeln Wir bieten professionelle Fliesenverlegung für Sie, um Ihrem Raum ein modernes und elegantes Aussehen zu ssehen</p>
+        <p className={styles.infoText}>{data.text}</p>
       </div>
       <ul className={styles.dataList}>
-        <li className={styles.dataItem}>
-          <p className={styles.dataNumber}>100+</p>
-          <p className={styles.dataText}>Objekte</p>
-        </li>
-        <li className={styles.dataItem}>
-          <p className={styles.dataNumber}>17</p>
-          <p className={styles.dataText}>Jahre Erfahrung</p>
-        </li>
-        <li className={styles.dataItem}>
-          <p className={styles.dataNumber}>100%</p>
-          <p className={styles.dataText}>Qualität</p>
-        </li>
+        {data?.statistics?.map(item=> <li className={styles.dataItem}>
+          <p className={styles.dataNumber}>{item.quantity}</p>
+          <p className={styles.dataText}>{item.direction}</p>
+        </li>)}        
       </ul>
     </div>
-  </section>;
+  </section>
 };
 
 
