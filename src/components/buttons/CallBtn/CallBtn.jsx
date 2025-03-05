3f8c04630bb79d3220f0403f4ad53afff07398d5
Nataliia <natalabojko@gmail.com>
2025-03-05 22:40:15 +0200

    footer&someStyles_fixing

--- src/components/buttons/CallBtn/CallBtn.jsx
+++ src/components/buttons/CallBtn/CallBtn.jsx
@@ -4,7 +4,7 @@ import styles from "./CallBtn.module.scss";
 
 const CallBtn = ({ id }) => {
   return (
-    <a href="tel:+380503738465" className={styles.callBtn} id={id}>
+    <a href="tel:+491701251314" className={styles.callBtn} id={id}>
       Rufen Sie uns an!
     </a>
   );
