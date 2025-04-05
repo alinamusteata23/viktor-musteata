7dbd21e21b77fdfa55de216f75b10a3b5e608fe4
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2025-04-06 01:14:35 +0300

    add tablet styles and fix sliders

--- src/sections/Testimonials/Testimonials.jsx
+++ src/sections/Testimonials/Testimonials.jsx
@@ -16,88 +16,63 @@ import { Navigation } from 'swiper/modules';
 
 import styles from './Testimonials.module.scss';
 import Image from 'next/image';
+import Loader from '@/components/Loader/Loader';
+import TestimonialsSlideItem from '@/components/TestimonialsSlideItem/TestimonialsSlideItem';
 
 const Testimonials = () => {
   const data = sectionsData.find((section) => section.name === 'Testimonials');
 
-  const firstRating = Math.round(data.reviews[0].rating);
-
-  const [swiperInstance, setSwiperInstance] = useState(null);
+  const [isLoad, setIsLoad] = useState(true);
 
   useEffect(() => {
-    if (!swiperInstance) return;
-
-    const updateSlides = () => {
-      const allSlides = document.querySelectorAll('.swiper-slide');
-
-      allSlides.forEach((slide, index) => {
-        slide.classList.remove('hidden-slide');
-
-        // Перевіряємо видимість слайду за допомогою swiper-slide-visible
-        if (
-          !slide.classList.contains('swiper-slide-visible') &&
-          (index + 1) % 3 === 0
-        ) {
-          slide.classList.add('hidden-slide');
-        }
-      });
-
-      swiperInstance.update();
-    };
-
-    swiperInstance.on('slideChangeTransitionEnd', updateSlides);
-    updateSlides();
-
-    return () => {
-      swiperInstance.off('slideChangeTransitionEnd', updateSlides);
-    };
-  }, [swiperInstance]);
+    setIsLoad(false);
+  }, []);
 
   return (
     <section className={`container ${styles.container}`}>
-      {data && (
+      {isLoad && (
+        <div className={styles.loaderWrapper}>
+          <Loader />
+        </div>
+      )}
+
+      {!isLoad && data && (
         <Swiper
           slidesPerView={1}
           spaceBetween={40}
-          navigation={{
-            clickable: true,
-          }}
+          navigation={{ clickable: true }}
           breakpoints={{
             320: {
               slidesPerView: 1,
               spaceBetween: 40,
             },
             768: {
-              slidesPerView: 2,
+              slidesPerView: 1,
               spaceBetween: 60,
             },
-            1366: {
+            1280: {
               slidesPerView: 2,
               spaceBetween: 40,
             },
           }}
           modules={[Navigation]}
-          onSwiper={setSwiperInstance}
           className="mySwiper"
-          observer={true} // Спостерігає за змінами
-          observeParents={true} // Спостерігає за батьківськими елементами
-          watchSlidesProgress={true} // Відстежує прогрес слайдів
         >
-          {data.reviews.map(({ author, rating, comment, id }) => (
-            <SwiperSlide key={id} className={`${styles.slide} swiper-slide`}>
-              <h3 className={styles.title}>{author}</h3>
-              <div className={styles.ratingContainer}>
-                {Array.from({ length: firstRating }).map((_, index) => (
-                  <div key={index} className={styles.svgContainer}>
-                    <Image src="/svgs/star.svg" alt="star" fill={true} />
-                  </div>
-                ))}
-                <p className={styles.rating}>{rating}</p>
-              </div>
-
-              <p className={styles.comment}>{comment}</p>
-            </SwiperSlide>
-          ))}
+          {data.reviews.map(({ author, rating, comment, id }) => {
+            const fullStars = Math.floor(rating);
+            const hasHalfStar = rating % 1 !== 0;
+            return (
+              <SwiperSlide key={id} className={`${styles.slide} swiper-slide`}>
+                <TestimonialsSlideItem
+                  author={author}
+                  rating={rating}
+                  comment={comment}
+                  fullStars={fullStars}
+                  hasHalfStar={hasHalfStar}
+                />
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       )}
     </section>
@@ -105,3 +80,88 @@ const Testimonials = () => {
 };
 
 export default Testimonials;
+
+// 'use client';
+
+// import React, { useEffect, useState } from 'react';
+// import { sectionsData } from '@/data/sectionsData';
+// import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/effect-coverflow';
+// import 'swiper/css/navigation';
+
+// import './Testimonials.css';
+
+// // import required modules
+// import { Navigation } from 'swiper/modules';
+
+// import styles from './Testimonials.module.scss';
+// import Image from 'next/image';
+// import Loader from '@/components/Loader/Loader';
+
+// const Testimonials = () => {
+//   const data = sectionsData.find((section) => section.name === 'Testimonials');
+
+//   // const firstRating = Math.round(data.reviews[0].rating);
+
+//   const [isLoad, setIsLoad] = useState(true);
+
+//   useEffect(() => {
+//     setIsLoad(false);
+//   }, []);
+
+//   return (
+//     <section className={`container ${styles.container}`}>
+//       {isLoad && (
+//         <div className={styles.loaderWrapper}>
+//           <Loader />
+//         </div>
+//       )}
+//       {!isLoad && data && (
+//         <Swiper
+//           slidesPerView={1}
+//           spaceBetween={40}
+//           navigation={{
+//             clickable: true,
+//           }}
+//           breakpoints={{
+//             320: {
+//               slidesPerView: 1,
+//               spaceBetween: 40,
+//             },
+//             768: {
+//               slidesPerView: 1,
+//               spaceBetween: 60,
+//             },
+//             1280: {
+//               slidesPerView: 2,
+//               spaceBetween: 40,
+//             },
+//           }}
+//           modules={[Navigation]}
+//           className="mySwiper"
+//         >
+//           {data.reviews.map(({ author, rating, comment, id }) => (
+//             <SwiperSlide key={id} className={`${styles.slide} swiper-slide`}>
+//               <h3 className={styles.title}>{author}</h3>
+//               <div className={styles.ratingContainer}>
+//                 {/* {Array.from({ length: firstRating }).map((_, index) => (
+//                   <div key={index} className={styles.svgContainer}>
+//                     <Image src="/svgs/star.svg" alt="star" fill={true} />
+//                   </div>
+//                 ))} */}
+//                 <p className={styles.rating}>{rating}</p>
+//               </div>
+
+//               <p className={styles.comment}>{comment}</p>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//       )}
+//     </section>
+//   );
+// };
+
+// export default Testimonials;
