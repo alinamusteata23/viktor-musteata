6c8df6581329eb46082bea9c0698cadebac5fd90
Nataliia <natalabojko@gmail.com>
2025-04-28 12:56:12 +0300

    ChangedSocLinks

--- src/data/socLinks.js
+++ src/data/socLinks.js
@@ -1,22 +1,25 @@
 export const socLinks = [
   {
-    title: "telegram",
-    href: "https://t.me/Natala_Boiko",
-    icon: "/sprite.svg#icon-telegram",
-  },
-  {
     title: "whatsapp",
-    href: "https://wa.me/380503738465",
+    href: "https://wa.me/491701251314",
     icon: "/sprite.svg#icon-whatsapp",
   },
-  {
-    title: "viber",
-    href: "viber://chat?number=380503738465",
-    icon: "/sprite.svg#icon-viber",
-  },
+
   {
     title: "facebook",
-    href: "https://www.facebook.com/natalabojko.bojko/",
+    href: "https://www.facebook.com/share/1AHgdjekBT/?mibextid=wwXIfr",
     icon: "/sprite.svg#icon-facebook",
   },
 ];
+
+// {
+//   title: "telegram",
+//   href: "https://t.me/Natala_Boiko",
+//   icon: "/sprite.svg#icon-telegram",
+// },
+
+// {
+//   title: "viber",
+//   href: "viber://chat?number=380503738465",
+//   icon: "/sprite.svg#icon-viber",
+// },
